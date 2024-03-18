cf8e6ceabc1712c438f59e278f8098c799de84e7
SHABARISH M <88005857+shabarish21@users.noreply.github.com>
2024-03-18 20:23:15 +0530

    Time: 769 ms (42.21%), Space: 27.6 MB (15.19%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,6 +1,7 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        min_price = prices[0]
+        #Profit occurs if the previous day stock price is lesser than the current. So we set minimum price as compare it with all day prices. same is done for max_price also.
+        min_price = prices[0] 
         max_profit = 0
         for i in prices[1:]:
             max_profit = max(max_profit,i-min_price)
